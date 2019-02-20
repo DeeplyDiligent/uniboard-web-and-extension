bef915f63147b367ec8ca1003d498d69479b3b62
Deep Bhattacharyya <bhattacharyya.deep@gmail.com>
2019-02-21 06:38:43 +1100

    colors and sidebar changed

--- src-web/components/nav/navbar.js
+++ src-web/components/nav/navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {isMobileOnly} from 'react-device-detect';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import * as firebase from "firebase/app";
+import LinkButton from '../styledComponents/linkButton';
 
 
 class Navbar extends Component {
@@ -23,14 +24,14 @@ class Navbar extends Component {
     render() { 
         this.expandedItemsAllClasses = "w-full "+this.state.expandedClass+" flex-grow lg:flex lg:items-center lg:w-auto"
         let logXButton = (<Link to="/login" className="block lg:inline-block lg:mt-0 text-purple hover:text-purple-light mr-6">
-            <NavBarButton text="Login" />
+            <LinkButton text="Login" />
         </Link>);
         let searchButton = false;
         let homeAndAboutButton = false
 
         if(this.props.loggedIn){
             logXButton = (<a href='javascript:void(0);' onClick = {this.logout} className="block lg:inline-block lg:mt-0 text-purple hover:text-purple-light mr-6">
-                <NavBarButton text="Logout" />
+                <LinkButton text="Logout" />
             </a>);
             searchButton = (<button className="flex items-center px-3 py-2 ml-2 border rounded text-purple border-purple hover:text-purple-lighter hover:border-purple-lighter">
                 <Link to='/home/search' className="text-purple">
@@ -40,10 +41,10 @@ class Navbar extends Component {
             homeAndAboutButton = (
             <React.Fragment>
                 <Link to="/app" className="block lg:inline-block lg:mt-0 text-purple hover:text-purple-light">
-                    <NavBarButton text="App" />
+                    <LinkButton text="App" />
                 </Link>
                 <Link to="/about" className="block lg:inline-block lg:mt-0 text-purple hover:text-purple-light">
-                    <NavBarButton text="About" />
+                    <LinkButton text="About" />
                 </Link>
             </React.Fragment>)
         }
@@ -71,7 +72,7 @@ class Navbar extends Component {
                     </Link> */}
                     {homeAndAboutButton}
                     <a href="https://chrome.google.com/webstore/detail/synopsis-your-moodle-hero/ikffeebmjnccffbijpcnlhadgdebadmc" className="block lg:inline-block lg:mt-0 text-purple hover:text-purple-light">
-                        <NavBarButton text="Get The Extension" />
+                        <LinkButton text="Get The Extension" />
                     </a>
                     {logXButton}
                     </div>
@@ -80,11 +81,5 @@ class Navbar extends Component {
         );
     }
 }
-
-const NavBarButton = (props) => (
-    <button className="bg-white hover:bg-purple-lightest text-purple font-bold py-1 px-3 rounded mt-3 lg:mt-0 focus:outline-none">
-        {props.text}
-    </button>
-)
  
 export default Navbar;
