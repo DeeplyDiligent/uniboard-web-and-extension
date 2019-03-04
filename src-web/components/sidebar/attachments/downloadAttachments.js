9162a02db0119423f5f30928b357926e298e7a44
Deep Bhattacharyya <bhattacharyya.deep@gmail.com>
2019-03-05 07:02:17 +1100

    new expanding algorithm and welcome modal changed

---
+++ src-web/components/sidebar/attachments/downloadAttachments.js
@@ -0,0 +1,30 @@
+// /* global chrome */
+// import React, { Component } from "react";
+// import LinkButton from "../../styledComponents/linkButton";
+// // if (typeof(chrome) !== 'undefined'){
+// //   const download = require("../../../download");
+// // }
+
+// class DownloadAttachments extends Component {
+//   state = {};
+//   download = () => {
+//     if (typeof(download) !== 'undefined'){
+//       download.startDownload(this.props.unitData,this.shortUnitName);
+//     }
+//   }
+//   render() {
+//     return (
+//       <div onClick={this.download} className="flex flex-col">
+//         <LinkButton
+//           text={
+//             <div>
+//               Download All&emsp;<i class="fas fa-download" />
+//             </div>
+//           }
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// export default DownloadAttachments;
